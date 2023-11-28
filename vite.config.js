783f1432f7e660e94f838370f673292e41c9ded1
JerryDJX <jerrydengsh@hotmail.com>
2023-11-28 20:56:32 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3d-demo/',
+  base: '/',
     build: {
         chunkSizeWarningLimit: 1500,
         rollupOptions: {
